c58649c4e709f61b56b8f3118e8df94ab9c64f19
Gustavo Dantas <dantas2509@gmail.com>
2020-08-26 10:40:36 -0300

    Filter Pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,8 +1,13 @@
 def is_pangram(sentence):
-    alphabet = list(range(ord('a'),ord('z')+1))
+    import string
+    alphabet = list(string.ascii_lowercase)
     
-    for l in sentence.lower():
-        if ord(l) in alphabet:
-            alphabet.remove(ord(l))
+    alphabet = ''.join(filter(lambda x: x not in sentence.lower(), alphabet))
+    
+    #alphabet = list(range(ord('a'),ord('z')+1))
+    
+    #for l in sentence.lower():
+    #    if ord(l) in alphabet:
+    #        alphabet.remove(ord(l))
     
     return len(alphabet) == 0
