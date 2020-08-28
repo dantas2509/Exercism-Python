6476a9027fb24b113f26de280ecd985e2c494e8a
Gustavo Dantas <dantas2509@gmail.com>
2020-08-28 13:48:32 -0300

    Resistor Color Duo

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,4 @@
+CATALOGUE = dict(black=0, brown=1, red=2, orange=3, yellow=4, green=5, blue=6, violet=7, grey=8, white=9)
+
+def value(colors):
+    return int(''.join([CATALOGUE[c] for c in colors]))
